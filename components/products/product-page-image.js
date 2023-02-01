14decca04ed45b3ef9128e1a9089e6ef199ed896
danialbagheri <bagheri.danial@gmail.com>
2023-02-01 14:27:02 +0000

    improving product image slider for product page

--- components/products/product-page-image.js
+++ components/products/product-page-image.js
@@ -1,45 +1,51 @@
-import { useState, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import {useState, useEffect} from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 export default function ProductPageImage(props) {
-  const [nav1, setNav1] = useState(null);
-  const [nav2, setNav2] = useState(null);
-  let slider1 = [];
-  let slider2 = [];
-  let imageList = props.imageList;
+  const [nav1, setNav1] = useState(null)
+  const [nav2, setNav2] = useState(null)
+  let slider1 = []
+  let slider2 = []
+  let imageList = props.imageList
 
   function sortByImageType(a, b) {
     if (a.main) {
-      return -2;
+      return -2
     }
-    if (a.image_type === "PI") {
-      return -1;
+    if (a.image_type === 'PI') {
+      return -1
     }
-    return 0;
+    return 0
   }
   // sorting the images by their type - Product Images first
-  imageList.sort(sortByImageType);
+  imageList.sort(sortByImageType)
 
   function searchForVariant(variantId, images) {
+    let variantImages = []
     for (var i = 0; i < images.length; i++) {
       if (images[i].variant === variantId) {
-        return i;
+        if (images[i].main) {
+          variantImages.unshift(i) // sends the main image to the begining of the array
+        } else {
+          variantImages.push(i)
+        }
       }
     }
+    return variantImages[0]
   }
   // This hooks the thumbnails to product main slider
   useEffect(() => {
-    setNav1(slider1);
-    setNav2(slider2);
-  }, [slider1, slider2]);
+    setNav1(slider1)
+    setNav2(slider2)
+  }, [slider1, slider2])
 
   // when variant is changes, product image will also change
   useEffect(() => {
-    let slideNumber = searchForVariant(props.selectedVariant.id, imageList);
-    slider1.slickGoTo(slideNumber);
-  }, [props.selectedVariant]);
+    let slideNumber = searchForVariant(props.selectedVariant.id, imageList)
+    slider1.slickGoTo(slideNumber)
+  }, [props.selectedVariant])
 
   let images = imageList.map((image, index) => {
     return (
@@ -56,32 +62,27 @@ export default function ProductPageImage(props) {
           />
         </picture>
       </div>
-    );
-  });
+    )
+  })
   return (
     <div className="product-photo-holder">
-      <Slider
-        slidesToShow={1}
-        asNavFor={nav2}
-        ref={(slider) => (slider1 = slider)}
-        className=""
-      >
+      <Slider slidesToShow={1} asNavFor={nav2} ref={slider => (slider1 = slider)} className="main-image-slider">
         {images}
       </Slider>
       <Slider
         className="img-thumbnail-product"
         asNavFor={nav1}
-        ref={(slider) => (slider2 = slider)}
+        ref={slider => (slider2 = slider)}
         slidesToShow={6}
         infinite={false}
         swipeToSlide={true}
         focusOnSelect={true}
         lazyLoad={true}
-        centerPadding={"10px"}
+        centerPadding={'10px'}
         // {...settings}
       >
         {images}
       </Slider>
     </div>
-  );
+  )
 }
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,99 +1,86 @@
-import { useState, useEffect } from "react";
-import data from "../../data.json";
-import BreadCrumb from "../../components/common/breadcrumb";
-import ProductPageImage from "../../components/products/product-page-image";
-import ProductDescription from "../../components/products/product-description";
-import ProductTabs from "../../components/products/product-tabs";
-import Head from "next/head";
-import RelatedProduct from "../../components/products/related-products";
-import ProductReviews from "../../components/reviews/product-reviews";
-import QuestionAndAnswerRow from "../../components/question-and-answers/question-and-answers-row";
-import { useShopify } from "../../components/hooks";
-import ProductSchema from "../../components/seo/product-schema";
+import {useState, useEffect} from 'react'
+import data from '../../data.json'
+import BreadCrumb from '../../components/common/breadcrumb'
+import ProductPageImage from '../../components/products/product-page-image'
+import ProductDescription from '../../components/products/product-description'
+import ProductTabs from '../../components/products/product-tabs'
+import Head from 'next/head'
+import RelatedProduct from '../../components/products/related-products'
+import ProductReviews from '../../components/reviews/product-reviews'
+import QuestionAndAnswerRow from '../../components/question-and-answers/question-and-answers-row'
+import {useShopify} from '../../components/hooks'
+import ProductSchema from '../../components/seo/product-schema'
 
 function Product(props) {
-  const product = props.productData;
-  const childProducts = props.productData.variants;
-  const instagramPosts = product.variants.map((v) => v.instagram_posts);
-  const [selectedPrice, setPrice] = useState(
-    props.productData.variants[0].price
-  );
+  const product = props.productData
+  const childProducts = props.productData.variants
+  const instagramPosts = product.variants.map(v => v.instagram_posts)
+  const [selectedPrice, setPrice] = useState(props.productData.variants[0].price)
   const [selectedProduct, setSelectedProduct] = useState({
     sku: props.productData.variants[0].sku,
-  });
+  })
 
-  const [selectedChildVariation, setSelectedChildVariation] = useState(
-    props.productData.variants[0].name
-  );
-  const [selectedVariant, setSelectedVariant] = useState(
-    props.productData.variants[0]
-  );
-  const [selectedChild, setChild] = useState(props.productData.variants[0].sku);
-  const [stores, setWheretoBuyStores] = useState(
-    props.productData.variants[0].where_to_buy
-  );
-  const [shopifyState, setShopifyState] = useState(null);
+  const [selectedChildVariation, setSelectedChildVariation] = useState(props.productData.variants[0].name)
+  const [selectedVariant, setSelectedVariant] = useState(props.productData.variants[0])
+  const [selectedChild, setChild] = useState(props.productData.variants[0].sku)
+  const [stores, setWheretoBuyStores] = useState(props.productData.variants[0].where_to_buy)
+  const [shopifyState, setShopifyState] = useState(null)
 
-  const { fetchProductByQuery } = useShopify();
+  const {fetchProductByQuery} = useShopify()
 
   useEffect(() => {
     const query = {
       query: `variant:[slug:${selectedChild}]`,
-    };
-    const f = fetchProductByQuery(query);
-    f.then((f) => setShopifyState(f[0]));
-  }, [selectedChild]);
+    }
+    const f = fetchProductByQuery(query)
+    f.then(f => setShopifyState(f[0]))
+  }, [selectedChild])
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    const selectedProduct = childProducts.find(
-      (product) => product.sku === e.target.value
-    );
-    setSelectedVariant(selectedProduct);
-    setPrice(selectedProduct.price);
-    setWheretoBuyStores(selectedProduct.where_to_buy);
-    setChild(selectedProduct.sku);
-    setSelectedChildVariation(selectedProduct.name);
-    setSelectedProduct({ sku: selectedProduct.sku });
-  };
+  const handleChange = e => {
+    e.preventDefault()
+    const selectedProduct = childProducts.find(product => product.sku === e.target.value)
+    setSelectedVariant(selectedProduct)
+    setPrice(selectedProduct.price)
+    setWheretoBuyStores(selectedProduct.where_to_buy)
+    setChild(selectedProduct.sku)
+    setSelectedChildVariation(selectedProduct.name)
+    setSelectedProduct({sku: selectedProduct.sku})
+  }
 
   const breadCrumbPath = [
-    { name: "Home", url: "/" },
-    { name: "Products", url: "/products/" },
+    {name: 'Home', url: '/'},
+    {name: 'Products', url: '/products/'},
     {
       name: product.types[0],
       url: `/products/?category=${encodeURIComponent(product.types[0])}`,
     },
-    { name: product.slug, url: `/products/${product.slug}` },
-  ];
-  let imageList = [];
+    {name: product.slug, url: `/products/${product.slug}`},
+  ]
+  let imageList = []
 
-  let child = null;
+  let child = null
 
   if (childProducts.length === 1) {
     child = childProducts.map((child, index) => {
-      imageList = imageList.concat(child.image_list);
-      return <p key={index}>{child.name}</p>;
-    });
+      imageList = imageList.concat(child.image_list)
+      return <p key={index}>{child.name}</p>
+    })
   } else {
     let options = childProducts.map((child, index) => {
-      imageList = imageList.concat(child.image_list);
+      imageList = imageList.concat(child.image_list)
       return (
         <option value={child.sku} key={index}>
           {child.name}
-          {"    "}
+          {'    '}
           {child.size}
         </option>
-      );
-    });
+      )
+    })
     child = (
-      <select
-        className="ProductOptionSelector"
-        onChange={(e) => handleChange(e)}
-      >
+      <select className="ProductOptionSelector" onChange={e => handleChange(e)}>
         {options}
       </select>
-    );
+    )
   }
 
   const productDescription = (
@@ -104,7 +91,7 @@ function Product(props) {
       product={product}
       selectedVariant={selectedVariant}
     />
-  );
+  )
   return (
     <div>
       <ProductSchema product={product} selected={selectedProduct} />
@@ -124,14 +111,9 @@ function Product(props) {
         <div className="row productContainer">
           <div className="col-md-6 col-sm-6 col-xs-12">
             <BreadCrumb breadcrumbs={breadCrumbPath} />
-            <ProductPageImage
-              imageList={imageList}
-              selectedVariant={selectedVariant}
-            />
+            <ProductPageImage imageList={imageList} selectedVariant={selectedVariant} />
           </div>
-          <div className="col-md-6 col-sm-6 col-xs-12">
-            {productDescription}
-          </div>
+          <div className="col-md-6 col-sm-6 col-xs-12">{productDescription}</div>
         </div>
       </div>
       <section className="row product-second-row">
@@ -146,10 +128,7 @@ function Product(props) {
         </div>
       </section>
       <section className="faq-container">
-        <QuestionAndAnswerRow
-          productSlug={product.slug}
-          faq={product.faq_list}
-        />
+        <QuestionAndAnswerRow productSlug={product.slug} faq={product.faq_list} />
       </section>
       <RelatedProduct related={product.related_products} />
       <div id="readReviews" />
@@ -162,15 +141,15 @@ function Product(props) {
         reviewAverageScore={product.review_average_score}
       />
     </div>
-  );
+  )
 }
 
 export async function getStaticProps(context) {
-  const slug = context.params.slug;
-  const baseUrl = data.apiUrl;
-  const url = baseUrl + `products/single/${slug}/?resize_w=375`;
-  const res = await fetch(url);
-  const productData = await res.json();
+  const slug = context.params.slug
+  const baseUrl = data.apiUrl
+  const url = baseUrl + `products/single/${slug}/?resize_w=850`
+  const res = await fetch(url)
+  const productData = await res.json()
   // key is needed here
   return {
     props: {
@@ -178,43 +157,43 @@ export async function getStaticProps(context) {
       productData,
     },
     revalidate: 120,
-  };
+  }
 }
 
 async function getAllPages(pageCount, url) {
-  let pageNumber = 1;
-  let productResult = [];
+  let pageNumber = 1
+  let productResult = []
   for (pageNumber; pageNumber <= pageCount; pageNumber++) {
-    let paginatedUrl = url + `?page=${pageNumber}`;
-    const res = await fetch(paginatedUrl);
-    const products = await res.json();
-    productResult.push(products.results);
+    let paginatedUrl = url + `?page=${pageNumber}`
+    const res = await fetch(paginatedUrl)
+    const products = await res.json()
+    productResult.push(products.results)
   }
-  return productResult;
+  return productResult
 }
 
 export async function getStaticPaths() {
-  const baseUrl = data.apiUrl;
-  const url = baseUrl + `products/product/`;
-  const res = await fetch(url);
-  const products = await res.json();
-  const pageCount = Math.ceil(products.count / 10);
-  let productResult = await getAllPages(pageCount, url);
-  let slugPaths = [];
+  const baseUrl = data.apiUrl
+  const url = baseUrl + `products/product/`
+  const res = await fetch(url)
+  const products = await res.json()
+  const pageCount = Math.ceil(products.count / 10)
+  let productResult = await getAllPages(pageCount, url)
+  let slugPaths = []
   for (let i = 0; i < productResult.length; i++) {
-    let slugs = productResult[i].map((item) => {
+    let slugs = productResult[i].map(item => {
       return {
         params: {
           slug: item.slug,
         },
-      };
-    });
-    Array.prototype.push.apply(slugPaths, slugs);
+      }
+    })
+    Array.prototype.push.apply(slugPaths, slugs)
   }
   return {
     paths: slugPaths,
     fallback: false,
-  };
+  }
 }
 
-export default Product;
+export default Product
