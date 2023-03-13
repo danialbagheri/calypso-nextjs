03df57c3abd3571213a71c2517ca93d933fbf602
danialbagheri <bagheri.danial@gmail.com>
2023-03-13 14:00:51 +0000

    Collection to pass slides to the slider

--- pages/collections/[collectionName].js
+++ pages/collections/[collectionName].js
@@ -4,27 +4,23 @@ import BreadCrumb from '../../components/common/breadcrumb'
 import ProductRangeItem from '../../components/products/product-range/ProductRangeItem'
 import HomeSlider from '../../components/home/home-slider'
 //Services
-import {getCollection, getCollectionBanner} from 'services'
+import {getCollection} from 'services'
 
-export default function BackToSchool(props) {
-  const {collection, slides} = props
+export default function CollectionName(props) {
+  const {collection} = props
   const breadCrumbPath = [
     {name: 'Home', url: '/'},
     {name: 'Collections', url: '/collections/'},
     {name: collection.name, url: `/${collection.slug}/`},
     ``,
   ]
-  const collectionItems = collection ? (
-    collection.items.map((item, index) => {
-      return <ProductRangeItem product={item.item} key={index} />
-    })
-  ) : (
-    <h1>Collection not found</h1>
-  )
+  const collectionItems = collection.items.map((item, index) => {
+    return <ProductRangeItem product={item.item} key={index} />
+  })
 
   return (
     <div>
-      {slides ? <HomeSlider slides={slides} /> : null}
+      {collection.slider && collection.slider.slides > 1 ? <HomeSlider slides={collection.slider.slides} /> : null}
       <div className="container">
         <div style={{padding: 10}}>
           <BreadCrumb breadcrumbs={breadCrumbPath} />
@@ -46,12 +42,10 @@ export async function getStaticProps(context) {
   const {collectionName} = context.params
 
   const collection = await getCollection(collectionName)
-  const slides = await getCollectionBanner(collectionName)
 
   return {
     props: {
       collection: collection.status === 404 ? null : collection,
-      slides: slides.results.length ? slides.results : null,
     }, // will be passed to the page component as props
   }
 }
