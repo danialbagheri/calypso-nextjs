13dcb2cc9a442bce80ba2332b96a9ec0f73e547b
danialbagheri <bagheri.danial@gmail.com>
2023-07-14 15:06:36 +0100

    added the first slide to show in best seller collection if exists

--- components/home/bestsellers/best-seller.js
+++ components/home/bestsellers/best-seller.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 import {Box} from '@mui/material'
 
 import BestSellerItems from './BestSellerItems'
@@ -26,13 +26,62 @@ export default function BestSeller(props) {
               height: {xs: '500px', sm: '600px', lg: 'unset'},
             }}
           >
-            <Image
-              alt="Calypso Best Seller products"
-              fill
-              src={collection.image}
-              style={{objectFit: 'cover'}}
-              // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
+            {collection.slider?.slides?.length > 0 ? (
+              <Box className="banner">
+                <Link
+                  href={
+                    collection.slider.slides[0].link
+                      ? collection.slider.slides[0].link
+                      : '#'
+                  }
+                >
+                  <picture>
+                    <source
+                      media="(min-width: 1536px)"
+                      srcSet={collection.slider.slides[0].xl_image}
+                      type="image/png"
+                    />
+                    <source
+                      media="(min-width: 1200px)"
+                      srcSet={collection.slider.slides[0].lg_image}
+                      type="image/png"
+                      width="1536"
+                    />
+                    <source
+                      media="(min-width: 900px)"
+                      srcSet={collection.slider.slides[0].md_image}
+                      type="image/png"
+                      width="1200"
+                    />
+                    <source
+                      media="(min-width: 600px)"
+                      srcSet={collection.slider.slides[0].sm_image}
+                      type="image/png"
+                      width="900"
+                    />
+                    <source
+                      media="(min-width: 0px)"
+                      srcSet={collection.slider.slides[0].xs_image}
+                      type="image/png"
+                      width="600"
+                    />
+                    <img
+                      alt={collection.slider.slides[0].image_alt_text}
+                      className="hero-image"
+                      src={collection.slider.slides[0].md_image}
+                    />
+                  </picture>
+                </Link>
+              </Box>
+            ) : (
+              <Image
+                alt="Calypso Best Seller products"
+                fill
+                src={collection.image}
+                style={{objectFit: 'cover'}}
+                // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            )}
           </Box>
           <Box
             sx={{
