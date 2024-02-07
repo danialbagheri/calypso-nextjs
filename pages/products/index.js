68d5ef3d6b73c7446d76ae6e8ad4567185a972cc
e.khaleghian <e.khaleghian@rooberah.co>
2024-02-07 15:24:46 +0330

    video size, product finder banner row, product hover style

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from 'react'
+import {useContext, useEffect} from 'react'
 
 import 'react-tabs/style/react-tabs.css'
 import Head from 'next/head'
@@ -8,18 +8,15 @@ import {getProducts, getProductsWithPagination} from 'services'
 import {AppContext} from '../../components/appProvider/AppProvider'
 import {getFavoriteProductsHandler} from '..'
 import {useRouter} from 'next/router'
-import {Box, Button, Typography} from '@mui/material'
+import {Box, Typography} from '@mui/material'
 import {getCollectionBanner} from '../../services'
 
-const PRODUCTS_PER_PAGE = 10
+// const PRODUCTS_PER_PAGE = 10
 const LG_IMAGE = 'lg_image'
 const MD_IMAGE = 'md_image'
 const MOBILE_IMAGE = 'mobile_webp'
 
 function Products(props) {
-  const [limit, setLimit] = useState(PRODUCTS_PER_PAGE)
-  const [showLoadMoreBtn, setShowLoadMoreBtn] = useState(true)
-
   const [appState, setAppState] = useContext(AppContext)
   const router = useRouter()
 
@@ -51,31 +48,11 @@ function Products(props) {
     category,
   })
 
-  function loadMore() {
-    const newLimit = limit + 10
-    if (newLimit >= products.length) {
-      setLimit(products.length)
-      setShowLoadMoreBtn(false)
-    } else {
-      setLimit(newLimit)
-    }
-  }
-
   const queryChangeHandler = async () => {
-    const limitCheckHandler = () => {
-      if (products.length <= limit) {
-        setShowLoadMoreBtn(false)
-      } else {
-        setShowLoadMoreBtn(true)
-      }
-    }
-
     try {
       await getFavoriteProductsHandler(setAppState)
-      limitCheckHandler()
     } catch (err) {
       console.error(err)
-      limitCheckHandler()
     }
   }
 
@@ -87,10 +64,6 @@ function Products(props) {
 
   useEffect(() => {
     queryChangeHandler()
-  }, [router.query.category, limit])
-
-  useEffect(() => {
-    setLimit(PRODUCTS_PER_PAGE)
   }, [router.query.category])
 
   return (
@@ -126,11 +99,7 @@ function Products(props) {
 
         <div>
           {products.length ? (
-            <ProductRange
-              banner={productFinderBannerSrc}
-              limit={limit}
-              products={products}
-            />
+            <ProductRange banner={productFinderBannerSrc} products={products} />
           ) : (
             <Box className="centralize" sx={{width: '100%', my: 20}}>
               <Typography fontSize={22} fontWeight={600}>
@@ -138,40 +107,6 @@ function Products(props) {
               </Typography>
             </Box>
           )}
-          <Box className="centralize">
-            {showLoadMoreBtn ? (
-              <Button
-                onClick={loadMore}
-                sx={{
-                  position: 'relative',
-
-                  textAlign: 'center',
-                  fontSize: '18px',
-                  fontStyle: 'normal',
-                  fontWeight: 600,
-                  lineHeight: 'normal',
-                  textTransform: 'none',
-
-                  p: '3px 80px',
-
-                  borderRadius: '10px',
-
-                  boxShadow: 'none',
-
-                  whiteSpace: 'nowrap',
-
-                  '&:hover': {
-                    bgcolor: '#FFF !important',
-                    boxShadow: 'none !important',
-                  },
-                  mb: 25,
-                }}
-                variant="outlined"
-              >
-                Load more
-              </Button>
-            ) : null}
-          </Box>
         </div>
       </section>
     </div>
