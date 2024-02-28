bb063ef5a45b7f208df0a1974a6e44f14374816c
e.khaleghian <e.khaleghian@rooberah.co>
2024-02-28 19:37:21 +0330

    fix: home page client side fetch

--- pages/index.js
+++ pages/index.js
@@ -11,38 +11,15 @@ import {
   StaySafe,
   Trending,
 } from 'components'
-
-import {
-  getBestSellerResults,
-  getCollectionBanner,
-  getTrendingUrls,
-} from 'services'
-
 import {AppContext} from '../components/appProvider/AppProvider'
-
 import {useAuthFetch} from 'components/customHooks'
-import {assetsEndPoints, getAssets, getFavoriteProductsHandler} from 'utils'
+import {getFavoriteProductsHandler} from 'utils'
 
-function Home(props) {
-  const {slides, isLoaded, trending, bestseller, secondarySlides, icons} = props
-
-  /* ---------------------------------- Hooks --------------------------------- */
-  const [, setAppState] = React.useContext(AppContext)
+function Home() {
   const authFetchHandler = useAuthFetch()
-  /* -------------------------------------------------------------------------- */
+  const [, setAppState] = React.useContext(AppContext)
 
   React.useEffect(() => {
-    //Get user account icon
-    if (icons[assetsEndPoints.userAccount]) {
-      setAppState(prevState => ({
-        ...prevState,
-        icons: {
-          ...prevState.icons,
-          [assetsEndPoints.userAccount]: icons[assetsEndPoints.userAccount],
-        },
-      }))
-    }
-
     //Get user favorite products
     getFavoriteProductsHandler({setAppState, authFetchHandler})
   }, [])
@@ -79,13 +56,10 @@ function Home(props) {
 
       <main>
         <section className="top-0">
-          <HomeSlider isLoaded={isLoaded} slides={slides} />
-
-          <Trending trending={trending} />
-          {secondarySlides ? (
-            <HomeSlider isLoaded={isLoaded} second slides={secondarySlides} />
-          ) : null}
-          <BestSeller bestseller={bestseller} />
+          <HomeSlider />
+          <Trending />
+          <HomeSlider isSecondBanner />
+          <BestSeller />
           <StaySafe />
           <AsSeen />
           <BlogSlider />
@@ -94,48 +68,6 @@ function Home(props) {
       </main>
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const props = {
-    slides: [],
-    secondarySlides: [],
-    isLoaded: false,
-    trending: [],
-    bestseller: {},
-  }
-  const promises = [
-    getCollectionBanner('homepage'),
-    getCollectionBanner('secondary'),
-    getTrendingUrls(),
-    getBestSellerResults(),
-  ]
-  const results = await Promise.allSettled(promises)
-  const icons = await getAssets([assetsEndPoints.userAccount])
-
-  results.forEach((res, i) => {
-    if (res.status === 'fulfilled') {
-      switch (i) {
-        case 0:
-          props.slides = res.value.results
-          break
-        case 1:
-          props.secondarySlides = res.value.results
-          break
-        case 2:
-          props.trending = res.value.items
-          break
-        case 3:
-          props.bestseller = res.value
-          break
-      }
-    }
-  })
-
-  return {
-    props: {...props, isLoaded: true, icons},
-    revalidate: 120, // will be passed to the page component as props
-  }
 }
 
 export default Home
