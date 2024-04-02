12c82438ac6fb1490f0a67df837affe42075ce9c
e.khaleghian <e.khaleghian@rooberah.co>
2024-04-02 18:56:23 +0330

    Refactor: icons

--- constants/index.js
+++ constants/index.js
@@ -1,1 +1,2 @@
 export * from './servicesConstants'
+export * from './user'
---
+++ constants/user.js
@@ -0,0 +1,23 @@
+export const createAccountBenefits = [
+  {
+    id: 8,
+    text: '10% off on you first purchase',
+    icon: null,
+    url: '',
+    position: 1,
+  },
+  {
+    id: 9,
+    text: 'Manage your orders',
+    icon: null,
+    url: '',
+    position: 1,
+  },
+  {
+    id: 5,
+    text: 'Process returns & refunds',
+    icon: null,
+    url: '',
+    position: 1,
+  },
+]
