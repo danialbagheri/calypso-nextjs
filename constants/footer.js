ee4838b73bedec1cb4633eeaaf108cacb92e1159
e.khaleghian <e.khaleghian@rooberah.co>
2024-04-04 17:19:01 +0330

    fix blog slider margin and foote

---
+++ constants/footer.js
@@ -0,0 +1,82 @@
+export const siteMapData = [
+  {
+    id: 'site_map_col_1',
+    items: [
+      {
+        id: 'sun_protection',
+        text: 'Sun protection',
+        link: '/products/categories/sun-protection',
+      },
+      {
+        id: 'kids',
+        text: 'Kids',
+        link: '/products/categories/kids',
+      },
+      {
+        id: 'health_care',
+        text: 'Health care',
+        link: '/products/categories/health-care',
+      },
+      {
+        id: 'after_sun',
+        text: 'After sun',
+        link: '/products/categories/after-sun',
+      },
+      {
+        id: 'tanning',
+        text: 'Tanning',
+        link: '/products/categories/tanning',
+      },
+    ],
+  },
+  {
+    id: 'site_map_col_2',
+    items: [
+      {
+        id: 'be_sun_reade',
+        text: 'Be sun ready',
+        link: '/be-sun-ready',
+      },
+      {
+        id: 'advice',
+        text: 'Advice',
+        link: '/advice',
+      },
+      {
+        id: 'about_us',
+        text: 'About us',
+        link: '/about',
+      },
+      {
+        id: 'contact_us',
+        text: 'Contact us',
+        link: '/contact-us',
+      },
+    ],
+  },
+  {
+    id: 'site_map_col_3',
+    items: [
+      {
+        id: 'faq',
+        text: 'FAQ',
+        link: '/faq',
+      },
+      {
+        id: 'terms_and_conditions',
+        text: 'Terms & conditions',
+        link: '/terms-conditions',
+      },
+      {
+        id: 'privacy_policy',
+        text: 'Privacy policy',
+        link: '/privacy-policy',
+      },
+      {
+        id: 'returns_policy',
+        text: 'Returns policy',
+        link: '/returns-policy',
+      },
+    ],
+  },
+]
